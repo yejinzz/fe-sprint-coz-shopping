d2717f01432091ee8d423c1a3d98743a321b43f5
yejinzz <yejin1320@naver.com>
2023-08-07 01:21:32 +0900

    메인 페이지 수정

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,26 +1,15 @@
-import ItemList from '../components/ItemList'; 
-import { styled } from 'styled-components';
+import Bookmarks from "../components/Bookmarks";
+import ItemList from "../components/ItemList";
+import { styled } from "styled-components";
 
+const MainContainer = styled.div``;
 
-const MainContainer = styled.div`
-
-    #subtitle{
-        font-size: 25px;
-        font-weight: bold;
-        margin: 20px 70px
-    };
-`;
-
-
-function Main () {
-
-    return(
-        <MainContainer>
-            <div id="subtitle"> 상품 리스트 </div>
-            <ItemList />
-            <div id="subtitle"> 북마크 리스트 </div>
-        </MainContainer>
-    )
-
+function Main() {
+  return (
+    <MainContainer>
+      <ItemList />
+      <Bookmarks />
+    </MainContainer>
+  );
 }
 export default Main;
